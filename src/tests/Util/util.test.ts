9e3174b4806191bc22be9bfa18dd2a2feaf576de
rockbender <rishisg@gmail.com>
2020-05-05 20:22:16 -0700

    add tests for util class

---
+++ src/tests/Util/util.test.ts
@@ -0,0 +1,24 @@
+import { getFormattedDuration } from "../../Util/util";
+
+describe('test util functions', () => {
+    it('expect only mins', async () => {
+        const duration = 60 * 1000
+        const actual = getFormattedDuration(duration);
+
+        expect(actual).toBe("01 minutes");
+    });
+
+    it('expect hour and mins', () => {
+        const duration = 60 * 1000 * 60;
+        const actual = getFormattedDuration(duration);
+
+        expect(actual).toBe("1h 00m");
+    })
+
+    it('expect 00 minutes', () => {
+        const duration = 45 * 1000;
+        const actual = getFormattedDuration(duration);
+
+        expect(actual).toBe("00 minutes");
+    })
+});
