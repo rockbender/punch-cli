e444b4010cfb7603273eff245e9468573d95a587
rockbender <rishisg@gmail.com>
2020-05-17 19:05:44 -0700

    status tests

--- src/tests/Commands/status.test.ts
+++ src/tests/Commands/status.test.ts
@@ -1,13 +1,41 @@
 
 import StatusCommand from '../../commands/status';
+import DataService from '../../Store/DataService';
+import { mocked } from 'ts-jest/utils';
+
+jest.mock('../../Store/DataService');
 
 describe('status command tests', () => {
-    it('does return', async () => {
-        let spy = jest.spyOn(process.stdout, 'write');
 
-		await StatusCommand.run([]);
-        expect(spy).toHaveBeenCalled(); //Update this to toHaveBeenCalledWith(expected print out)
-        
-        // find a way to test he condition and fake the dataservice as well as check only one output instead of all output.
+    const dataServiceMock = {
+        getSessionLogs: jest.fn().mockReturnValue([]),
+        getSession: jest.fn().mockReturnValue({
+            StartDateTime: (new Date()).toISOString(),
+            Notes: ''
+        } as ISession)
+    } as unknown as DataService;
+
+    beforeEach(() => {
+        mocked(DataService).mockImplementation(() => {
+            return dataServiceMock
+        })
+    });
+
+    it('session not null expect logs not called', async () => {
+
+        await StatusCommand.run([]);
+
+        expect(dataServiceMock.getSession).toBeCalledTimes(1);
+        expect(dataServiceMock.getSessionLogs).toHaveBeenCalledTimes(0);
     })
-})
+
+    it('session null expect logs called', async () => {
+
+        dataServiceMock.getSession = jest.fn().mockReturnValue(null);
+        
+        await StatusCommand.run([]);
+
+        expect(dataServiceMock.getSession).toBeCalledTimes(1);
+        expect(dataServiceMock.getSessionLogs).toHaveBeenCalledTimes(1);
+    })
+}) 
