ea49f19d372837ee17fca97df2efe0d9ec1dc7e5
rockbender <rishisg@gmail.com>
2020-05-19 19:35:20 -0700

    in test - add after test cleanup

--- tests/Commands/in.test.ts
+++ tests/Commands/in.test.ts
@@ -6,28 +6,24 @@ import { mocked } from 'ts-jest/utils';
 jest.mock('../../src/Store/DataService');
 
 describe('test in command', () => {
-    const dataServiceMock = {
-        getSession: jest.fn().mockReturnValue({
-            StartDateTime: '2020-05-01T10:30:00.500Z'
-        } as ISession),
-        addSession: jest.fn(),
-        close: jest.fn()
-    } as unknown as DataService;
+    let dataServiceMock = {} as DataService;
 
     beforeEach(() => {
-        mocked(DataService).mockImplementationOnce(() => {
+        dataServiceMock = {
+            getSession: jest.fn().mockReturnValue({
+                StartDateTime: '2020-05-01T10:30:00.500Z'
+            }),
+            addSession: jest.fn(),
+            close: jest.fn()
+        } as unknown as DataService;
+
+        mocked(DataService).mockImplementation(() => {
             return dataServiceMock
         })
     });
 
-    //TODO Rishi - When the order of these tests are changed, running suite test fails. Check mock clear works
-
-    it('current session expect session not created', async () => {
-
-        await InCommand.run([]);
-
-        expect(dataServiceMock.getSession).toBeCalledTimes(1);
-        expect(dataServiceMock.addSession).toBeCalledTimes(0);
+    afterEach(() => {
+        jest.clearAllMocks();
     })
 
     it('current session null expect session created', async () => {
@@ -37,5 +33,13 @@ describe('test in command', () => {
 
         expect(dataServiceMock.getSession).toBeCalledTimes(1);
         expect(dataServiceMock.addSession).toBeCalledTimes(1);
+    })
+
+    it('current session expect session not created', async () => {
+
+        await InCommand.run([]);
+
+        expect(dataServiceMock.getSession).toBeCalledTimes(1);
+        expect(dataServiceMock.addSession).toBeCalledTimes(0);
     })
 });
