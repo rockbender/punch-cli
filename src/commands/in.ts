3cbb6cd9aa292bfa43895c570fc191e3b50386d2
rockbender <rishisg@gmail.com>
2020-06-02 18:26:44 -0700

    in - fix amend usage, add description

--- src/commands/in.ts
+++ src/commands/in.ts
@@ -8,9 +8,9 @@ export default class InCommand extends Command {
   static description = 'Start a new session. Use -f to restart current session.';
   static aliases = ['i'];
   static flags = {
-    force: flags.boolean({char: 'f'}), // (-f, --force)
-    message: flags.string({char: 'm'}),
-    amend: flags.string({char: 'a'})
+    force: flags.boolean({char: 'f', description: 'Force re-start a new session. Discards an active session if found.'}),
+    message: flags.string({char: 'm', description: 'Set note. Can only be used while creating a new session.'}),
+    amend: flags.string({char: 'a', description: 'Amend comment to the current seesion. Cannot be used when creating a new session.'})
   };
   
   private db = new DataService();
@@ -26,25 +26,20 @@ export default class InCommand extends Command {
       this.doPunchIn(now);
 
     } else if (this.flags.amend) {
-      if(currentSession == null) {
-        this.log('\nNo active session found to amend. Try the command without -a');
-      } else {
-        const message = this.flags.amend == null ? '' : this.flags.amend;
-        this.db.updateCurrentSession(message);
+      const message = this.flags.amend == null ? '' : this.flags.amend;
+      this.db.updateCurrentSession(message);
 
-        const currentSession = this.db.getSession();
+      const currentSession = this.db.getSession();
 
-        this.log('\nCurrent session:\n');
-      
-        const now = new Date();
-        const startDateTime = new Date(currentSession.StartDateTime);
-  
-        this.log(chalk.yellow('\tDuration:\t'), chalk.yellow(startDateTime.duration(now)));
-        this.log(chalk.green('\tStart: \t\t'), chalk.green(startDateTime.formattedDateTime()));
-        this.log(chalk.green('\tEnd: \t\t'), chalk.green('-'));
-        this.log(chalk.green('\tNotes:\t\t'), chalk.green(currentSession.Notes === '' ? '-' : currentSession.Notes));
+      this.log('\nCurrent session:\n');
+    
+      const now = new Date();
+      const startDateTime = new Date(currentSession.StartDateTime);
 
-      }
+      this.log(chalk.yellow('\tDuration:\t'), chalk.yellow(startDateTime.duration(now)));
+      this.log(chalk.green('\tStart: \t\t'), chalk.green(startDateTime.formattedDateTime()));
+      this.log(chalk.green('\tEnd: \t\t'), chalk.green('-'));
+      this.log(chalk.green('\tNotes:\t\t'), chalk.green(currentSession.Notes === '' ? '-' : currentSession.Notes));
     } else {
       
       let startDateTime = new Date(currentSession.StartDateTime);
@@ -56,6 +51,10 @@ export default class InCommand extends Command {
   }
 
   doPunchIn(date: Date) {
+
+    if(this.flags.amend) {
+      this.log('\nFlag --amend ignored. Cannot be used when creating a new session.');
+    }
 
     const message = this.flags.message == null ? '' : this.flags.message;
 
